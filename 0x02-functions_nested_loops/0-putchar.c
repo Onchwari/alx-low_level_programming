3cf190c0afb68a9816e7c6c2298707680c667427
chelsea <mogorechelsea@gmail.com>
2022-03-16 02:21:06 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main-prints _putchar
+ * description: prints _putchar
+ * return: 0(success0
+ */
+
+int main(void)
+{
+	char text[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
