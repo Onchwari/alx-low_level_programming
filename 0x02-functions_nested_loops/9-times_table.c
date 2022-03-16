141ae6114d6db29d9fbd20b95232c83257f80d32
chelsea <mogorechelsea@gmail.com>
2022-03-16 05:33:45 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,7 +12,7 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j< 10; j++)
+		for (j = 0; j < 10; j++)
 		{
 			k = j * i;
 			if (j == 0)
@@ -26,7 +26,7 @@ void times_table(void)
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
-			}else if (k >= 10)
+			} else if (k >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
